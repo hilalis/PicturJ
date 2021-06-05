a80f5819f8b6c5e6036ead308751e50dddb1d688
hilalis <64924739+hilalis@users.noreply.github.com>
2021-06-05 16:55:54 +0300

    Releasing v0.2

---
+++ Form1.Designer.cs
@@ -0,0 +1,436 @@
+﻿//Developed by @hilalis
+namespace PicturJ
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.newProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.languagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.germanyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.label5 = new System.Windows.Forms.Label();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.русскийЯзыкToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.Color.White;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.newProjectToolStripMenuItem,
+									this.exportToolStripMenuItem,
+									this.languagesToolStripMenuItem,
+									this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1076, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// newProjectToolStripMenuItem
+			// 
+			this.newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
+			this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+			this.newProjectToolStripMenuItem.Text = "Open File";
+			this.newProjectToolStripMenuItem.Click += new System.EventHandler(this.NewProjectToolStripMenuItemClick);
+			// 
+			// exportToolStripMenuItem
+			// 
+			this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+			this.exportToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+			this.exportToolStripMenuItem.Text = "Export";
+			this.exportToolStripMenuItem.Click += new System.EventHandler(this.ExportToolStripMenuItemClick);
+			// 
+			// languagesToolStripMenuItem
+			// 
+			this.languagesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.englishToolStripMenuItem,
+									this.germanyToolStripMenuItem,
+									this.русскийЯзыкToolStripMenuItem});
+			this.languagesToolStripMenuItem.Name = "languagesToolStripMenuItem";
+			this.languagesToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+			this.languagesToolStripMenuItem.Text = "Languages";
+			// 
+			// englishToolStripMenuItem
+			// 
+			this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+			this.englishToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.englishToolStripMenuItem.Text = "English";
+			// 
+			// germanyToolStripMenuItem
+			// 
+			this.germanyToolStripMenuItem.Name = "germanyToolStripMenuItem";
+			this.germanyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.germanyToolStripMenuItem.Text = "Deutsch";
+			this.germanyToolStripMenuItem.Click += new System.EventHandler(this.GermanyToolStripMenuItemClick);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+			this.helpToolStripMenuItem.Text = "About";
+			this.helpToolStripMenuItem.Click += new System.EventHandler(this.HelpToolStripMenuItemClick);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Black;
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(22, 45);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(553, 413);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(23, 46);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(194, 41);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Monochrome";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Black;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(764, 541);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(219, 41);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Export";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label5
+			// 
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(93, 53);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(58, 22);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Text:";
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Items.AddRange(new object[] {
+									"Regular",
+									"Italic",
+									"Bold"});
+			this.comboBox3.Location = new System.Drawing.Point(157, 137);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(110, 21);
+			this.comboBox3.TabIndex = 8;
+			// 
+			// label3
+			// 
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(93, 137);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(58, 23);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Font Style:";
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// label2
+			// 
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(116, 106);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 23);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Font:";
+			// 
+			// label1
+			// 
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(92, 79);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 21);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Font Size:";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+									"Arial",
+									"Arial Black",
+									"Verdana",
+									"Calibri"});
+			this.comboBox2.Location = new System.Drawing.Point(157, 106);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(109, 21);
+			this.comboBox2.TabIndex = 4;
+			// 
+			// textBox1
+			// 
+			this.textBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+			this.textBox1.Location = new System.Drawing.Point(156, 53);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(109, 20);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.Text = "Your Text...";
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+									"8",
+									"10",
+									"12",
+									"14",
+									"16",
+									"18",
+									"20",
+									"22",
+									"24",
+									"26",
+									"28",
+									"30",
+									"32",
+									"34",
+									"36",
+									"38",
+									"40"});
+			this.comboBox1.Location = new System.Drawing.Point(156, 79);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(109, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.Black;
+			this.button5.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button5.ForeColor = System.Drawing.Color.White;
+			this.button5.Location = new System.Drawing.Point(259, 201);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(166, 39);
+			this.button5.TabIndex = 1;
+			this.button5.Text = "Apply";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Black;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button4.ForeColor = System.Drawing.Color.White;
+			this.button4.Location = new System.Drawing.Point(41, 205);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(142, 35);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "Choose Color...";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Black;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button3.ForeColor = System.Drawing.Color.White;
+			this.button3.Location = new System.Drawing.Point(518, 541);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(219, 41);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "Edit Picture";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Controls.Add(this.comboBox3);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.comboBox2);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.comboBox1);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Location = new System.Drawing.Point(597, 45);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(452, 271);
+			this.panel1.TabIndex = 8;
+			this.panel1.Visible = false;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(13, 14);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(231, 39);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Add Text";
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.pictureBox2);
+			this.panel2.Controls.Add(this.label6);
+			this.panel2.Controls.Add(this.button1);
+			this.panel2.Location = new System.Drawing.Point(597, 336);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(452, 151);
+			this.panel2.TabIndex = 9;
+			this.panel2.Visible = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(228, 46);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(39, 41);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 4;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Visible = false;
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label6.ForeColor = System.Drawing.Color.White;
+			this.label6.Location = new System.Drawing.Point(23, 15);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 3;
+			this.label6.Text = "Effects";
+			// 
+			// русскийЯзыкToolStripMenuItem
+			// 
+			this.русскийЯзыкToolStripMenuItem.Name = "русскийЯзыкToolStripMenuItem";
+			this.русскийЯзыкToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.русскийЯзыкToolStripMenuItem.Text = "Русский язык";
+			this.русскийЯзыкToolStripMenuItem.Click += new System.EventHandler(this.РусскийЯзыкToolStripMenuItemClick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(1076, 611);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "PicturJ";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.ToolStripMenuItem русскийЯзыкToolStripMenuItem;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.ToolStripMenuItem germanyToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem languagesToolStripMenuItem;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+		private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿//Developed by @hilalis
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+
+namespace PicturJ
+{
+	/// <summary>
+	/// Description of Form1.
+	/// </summary>
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		string dosyaYolu = string.Empty;
+		Bitmap bmp = null;	
+
+Color renk;
+		
+		void NewProjectToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+dosyaYolu = openFileDialog1.FileName;
+bmp = new Bitmap(dosyaYolu);
+pictureBox1.Image = bmp;
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			pictureBox2.Visible = true;
+			for(int y = 0; y<bmp.Height; y++)
+			{
+				for (int x = 0; x < bmp.Width; x++)
+				{
+					Color eski = bmp.GetPixel(x,y);
+					int ortalama = (eski.R + eski.G + eski.B) / 3;
+					Color yeni = Color.FromArgb(eski.A, ortalama, ortalama, ortalama);
+					bmp.SetPixel(x,y,yeni);
+				}
+			}
+			pictureBox2.Visible = false;
+			pictureBox1.Image = bmp;
+saveFileDialog1.Filter = "JPEG Picture|.jpg";
+saveFileDialog1.Title = "Export";
+ saveFileDialog1.ShowDialog();
+ if(saveFileDialog1.FileName!= "")
+ {
+ 	FileStream savefile = (FileStream)saveFileDialog1.OpenFile();
+ 	pictureBox1.Image.Save(savefile, System.Drawing.Imaging.ImageFormat.Jpeg);
+ }
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+saveFileDialog1.Filter = "JPEG Picture|.jpg";
+saveFileDialog1.Title = "Export Your Picture";
+ saveFileDialog1.ShowDialog();
+ if(saveFileDialog1.FileName!= "")
+ {
+ 	FileStream savefile = (FileStream)saveFileDialog1.OpenFile();
+ 	pictureBox1.Image.Save(savefile, System.Drawing.Imaging.ImageFormat.Jpeg);
+ }
+		}
+		
+		
+		
+		void ExportToolStripMenuItemClick(object sender, EventArgs e)
+		{
+saveFileDialog1.Filter = "JPEG Picture|.jpg";
+saveFileDialog1.Title = "Export Your Picture";
+ saveFileDialog1.ShowDialog();
+ if(saveFileDialog1.FileName!= "")
+ {
+ 	FileStream savefile = (FileStream)saveFileDialog1.OpenFile();
+ 	pictureBox1.Image.Save(savefile, System.Drawing.Imaging.ImageFormat.Jpeg);
+ }			
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+panel1.Visible=true;
+panel2.Visible=true;
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			colorDialog1.ShowDialog();
+			renk = colorDialog1.Color;
+		}
+		
+		void Button5Click(object sender, EventArgs e)
+		{
+			if(comboBox3.Text == "Regular")
+			{
+			if(renk != null && comboBox1.Text != "")
+			bmp = new Bitmap(dosyaYolu);
+			Graphics g = Graphics.FromImage(bmp);
+			g.DrawString(textBox1.Text,new Font (comboBox2.Text,Convert.ToInt32(comboBox1.Text), FontStyle.Regular), new SolidBrush(renk),10,10);
+			pictureBox1.Image = bmp;
+			}
+			if(comboBox3.Text == "Italic")
+			{
+			if(renk != null && comboBox1.Text != "")
+			bmp = new Bitmap(dosyaYolu);
+			Graphics g = Graphics.FromImage(bmp);
+			g.DrawString(textBox1.Text,new Font (comboBox2.Text,Convert.ToInt32(comboBox1.Text), FontStyle.Italic), new SolidBrush(renk),10,10);
+			pictureBox1.Image = bmp;
+			}
+			if(comboBox3.Text == "Bold")
+			{
+			if(renk != null && comboBox1.Text != "")
+			bmp = new Bitmap(dosyaYolu);
+			Graphics g = Graphics.FromImage(bmp);
+			g.DrawString(textBox1.Text,new Font (comboBox2.Text,Convert.ToInt32(comboBox1.Text), FontStyle.Bold), new SolidBrush(renk),10,10);
+			pictureBox1.Image = bmp;
+			}
+		}
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+							
+		}
+		
+		void Label3Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void GroupBox2Enter(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void HelpToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			Form2 about = new Form2();
+			about.Show();
+		 
+		}
+		
+		void GermanyToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			label1.Text = "Schriftgröße";
+			label2.Text = "Schriftart";
+			label3.Text = "Schriftstil";
+			newProjectToolStripMenuItem.Text = "Datei öffnen";
+			button5.Text = "anwenden";
+			label6.Text = "Auswirkungen";
+			button1.Text = "Monochrome";
+			languagesToolStripMenuItem.Text = "Sprachen";
+			helpToolStripMenuItem.Text = "Über";
+			button3.Text = "Bild bearbeiten";
+			saveFileDialog1.Title = "Export";
+			button4.Text = "wähle eine Farbe";
+			exportToolStripMenuItem.Text = "Ausfuhr";
+			button2.Text = "Ausfuhr";
+			label5.Text = "Wortlaut";
+			label4.Text = "Text hinzufügen";
+			button1.Text = "schwarzweiß";
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+	
+		
+		void РусскийЯзыкToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			button3.Text = "Редактировать картинку";
+		}
+	}
+}
